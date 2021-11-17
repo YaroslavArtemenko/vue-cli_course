d91faa694a2f18479bf85ff0e2b1ec73966f47a0
Yaros <Yaroslavhik@yandex.ua>
2021-11-17 18:09:34 +0100

    chenged <li>s on dynamical with massive of links

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,8 @@
 
             <ul class="navbar-list">
 
-              <li class="navbar-item">
-                <router-link href="" class="navbar-link" to="/">Home</router-link>
-              </li>
-
-              <li class="navbar-item">
-                <router-link href="" class="navbar-link" to="/example">Example</router-link>
+              <li class="navbar-item" v-for="link in links" :key="link.title">
+                <router-link href="" class="navbar-link" :title="link.title" :to="link.url">{{ link.title }}</router-link>
               </li>
 
             </ul>
@@ -32,7 +28,16 @@
 
 <script>
 
-export default {}
+export default {
+  data () {
+    return {
+      links: [
+        {title: 'Home', url: '/'},
+        {title: 'Example', url: '/example'},
+      ]
+    }
+  }
+}
 </script>
 
 <style>
