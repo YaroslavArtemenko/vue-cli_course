b61b6d4e9bb6ae606ff2955b932f497f92ecedc2
Yaros <Yaroslavhik@yandex.ua>
2021-11-17 18:05:49 +0100

    added router-link with navigation

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,26 @@
         <div class="container">
           <div class="navbar-content">
             <div class="logo">VUE-CLI</div>
+
             <ul class="navbar-list">
+
               <li class="navbar-item">
-                <a href="" class="navbar-link">Home</a>
+                <router-link href="" class="navbar-link" to="/">Home</router-link>
               </li>
+
               <li class="navbar-item">
-                <a href="" class="navbar-link">Example</a>
+                <router-link href="" class="navbar-link" to="/example">Example</router-link>
               </li>
+
             </ul>
+
           </div>
         </div>
       </div>
     </header>
 
     <router-view></router-view>
+
     <!--    <footer></footer>-->
   </div>
 </template>
