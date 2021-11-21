4f6a0dcaf1701220e0552631f17d41bdfd4412a7
Yaros <Yaroslavhik@yandex.ua>
2021-11-21 16:29:16 +0100

    added code to vuex-store and comments for it

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,29 @@ import Vuex from 'vuex';
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: {},
-    mutations: {},
-    actions: {},
-    getters: {},
+    // храним какие-то данные,
+    state: {
+        message: 'Hello vuex'
+    },
+    // служит для того, чтобы менять наш стейт
+    // payload - это то, что передаем в мутацию с action
+    mutations: {
+        setMessage (state) {
+            state.message = 'hello again'
+        }
+    },
+    // свойство, в котором мы можем изменять стейт, но не напряю, а используя мутации
+    // через коммит обращаемся к мутации
+    // нужен для того, чтобы работать с асинхронными данных, потому что должны ждать
+    actions: {
+        setMessage ({commit}) {
+            commit('setMessage')
+        }
+    },
+    // action and mutation = setters, getter - получаем значения
+    getters: {
+        getMessage (state) {
+            return state.message
+        }
+    },
 })
